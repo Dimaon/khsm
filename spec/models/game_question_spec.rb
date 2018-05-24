1b4b219c8ba494e2e954836539299933675ba1b7
dmitriy <kulikov.perm@gmail.com>
2018-05-25 01:48:46 +0300

    add test for helpers

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -41,4 +41,59 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.correct_answer_key).not_to eq 'a'
     end
   end
+
+
+  # help_hash у нас имеет такой формат:
+  # {
+  #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
+  #   audience_help: {'a' => 42, 'c' => 37 ...}, # Распределение голосов по вариантам a, b, c, d
+  #   friend_call: 'Василий Петрович считает, что правильный ответ A'
+  # }
+
+  context 'user helpers' do
+    it 'correct audience_help' do
+      # проверяем, что этой подсказки нету
+      expect(game_question.help_hash).not_to include(:audience_help)
+
+      game_question.add_audience_help
+
+      expect(game_question.help_hash).to include(:audience_help)
+      expect(game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    it '#help_hash return hash' do
+      expect(game_question.help_hash).to eq({})
+
+      game_question.help_hash[:first_key] = 'first value'
+      game_question.help_hash[:last_key] = 'last value'
+
+      expect(game_question.save).to be_truthy
+
+      gq_from_db = GameQuestion.find(game_question.id)
+
+      expect(gq_from_db.help_hash).to eq({first_key: 'first value', last_key: 'last value'})
+    end
+
+    it '#50/50' do
+      # проверяем, что этой подсказки нету
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+
+      game_question.add_fifty_fifty
+
+      expect(game_question.help_hash).to include(:fifty_fifty)
+      expect(game_question.help_hash[:fifty_fifty].size).to eq(2)
+      expect(game_question.help_hash[:fifty_fifty]).to include('b')
+    end
+
+    it '#friend_call' do
+      # проверяем, что этой подсказки нету
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+
+      expect(game_question.help_hash).to include(:friend_call)
+      expect(game_question.help_hash[:friend_call]).to be
+    end
+  end
+
 end
